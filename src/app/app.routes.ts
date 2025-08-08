8c6fe0dcf23c7302569b0a2fd9244d8a10942180
marc-tg <marctg2005@gmail.com>
2025-08-09 00:31:01 +0200

    Added buttons from home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { PruebaComponent } from './components/prueba/prueba.component';
 import { AuthPageComponent } from './components/auth-page/auth-page.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path: '', component: AuthPageComponent},
+    {path: 'home', component: HomeComponent},
+
 ];
